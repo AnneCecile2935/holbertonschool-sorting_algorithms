084f16b0f56c2408ec96ddc471cd15e8344ca61b
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-09 17:03:05 +0200

    update task 2 with betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,18 +1,25 @@
 #include "sort.h"
+/**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: pointer to the array
+ * @size: size of the array
+ * Return: nothing
+ */
 void selection_sort(int *array, size_t size)
 {
-	size_t i;
-	size_t j;
-	size_t min_idx;
-	int temp;
-	if (array == NULL || size < 2)
-	return;
-for (i = 0; i < size -1; i++)
+size_t i;
+size_t j;
+size_t min_idx;
+int temp;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
 {
 	min_idx = i;
-	for (j = i+1; j < size; j++)
+	for (j = i + 1; j < size; j++)
 	{
-		if (array[j] < array[min_idx])
+	if (array[j] < array[min_idx])
 	min_idx = j;
 	}
 	if (min_idx != i)
