90e38b55e0357572a5da508d5e2884b69ab31122
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-09 17:00:39 +0200

    update task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t min_idx;
+	int temp;
+	if (array == NULL || size < 2)
+	return;
+for (i = 0; i < size -1; i++)
+{
+	min_idx = i;
+	for (j = i+1; j < size; j++)
+	{
+		if (array[j] < array[min_idx])
+	min_idx = j;
+	}
+	if (min_idx != i)
+	{
+		temp = array[i];
+		array[i] = array[min_idx];
+		array[min_idx] = temp;
+		print_array(array, size);
+	}
+}
+}
