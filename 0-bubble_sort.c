de9f28181a50f4bc420a282931877a75f2dee4ed
AnneCecile2935 <10435@holbertonstudents.com>
2025-04-10 11:41:10 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,14 +26,15 @@ void bubble_sort(int *array, size_t size)
 size_t i, j;
 if (!array)
 return;
-			for (i = 0; i < size - 1; i++)
+			for (i = 0; i < size - 1; i++) /** boucle extérieure pour parcourir le tableau (chaque tour de tableau) => size -1, si tableau de 3 cases on fera 2 tours par exemple*/
 		{
-			for (j = 0; j < size - i - 1; j++)
+			for (j = 0; j < size - i - 1; j++) /** boucle intérieure pour comparer chaque paire d'éléments qui ne sont pas encore triés => size - i -1, si on est au tour 1, on va comparer les deux premier éléments donc j-1-1 
+   au tour suivant, on ne comparera pas les valeurs qui ont déjà été comparées*/
 			{
-				if (array[j] > array[j + 1])
+				if (array[j] > array[j + 1]) /** si la valeur trouvé de j est supérieure à la valeur trouvée de j+1*/
 				{
-					swap(&array[j], &array[j + 1]);
-					print_array(array, size);
+					swap(&array[j], &array[j + 1]); /** on échange les valeurs de j et j+1 en leur changeant leur adresses*/
+					print_array(array, size); /** afficher le tableau après échange*/
 				}
 			}
 		}
